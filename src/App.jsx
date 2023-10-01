4251d6724a36b28d5ecbd5fc5578a23ec9bfe1c2
iman ismail <imanismailbox@gmail.com>
2023-10-01 23:39:33 +0700

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
+import ReactAudioPlayer from 'react-audio-player';
+
+import bgSound from './assets/jannat64.mp3'
 import config from './config.json';
 
-import Navigation from './components/Navigation';
-
 import Pembuka from './sections/Pembuka';
+import Ayat from './sections/Ayat';
 import Pengantin from './sections/Pengantin';
 import Acara from './sections/Acara';
-import Ayat from './sections/Ayat';
+import BukuTamu from './sections/BukuTamu'
 import Penutup from './sections/Penutup';
 
 function App() {
@@ -16,12 +18,25 @@ function App() {
   const [tujuan, setTujuan] = useState('Tamu Istimewa')
   const [data, setData] = useState(config);
   const [opening, setOpening] = useState(true);
+  const [isChecked, setIsChecked] = useState(false);
+  const [autoplay, setAutoplay] = useState(true);
+  const [isPlaying, setIsPlaying] = useState(true);
+  const player = useRef()
   
   useEffect(()=>{
     if (kepada) {
       setTujuan(kepada)
     }
   }, [kepada])
+
+  const togglePlay = () => {
+    setIsPlaying(!isPlaying)
+    if (isPlaying) {
+      player.current.audioEl.current.pause()
+    } else {
+      player.current.audioEl.current.play()
+    }
+  }
 
   return (
     <>
@@ -69,11 +84,29 @@ function App() {
           <Ayat />
           <Pengantin pria={data.pria} wanita={data.wanita} />
           <Acara acara={data.acara} />
+          <BukuTamu />
           <Penutup data={data} />
         </>
         )}
       </div>
-      <Navigation className="z-50" />
+      <div title="Play/Pause Music"
+        className="fixed z-50 bottom-1/3 left-4">
+        <label className="swap items-center justify-center w-10 h-10 font-medium text-white bg-primary rounded-full hover:bg-primary-700 group hover:shadow-lg hover:shadow-primary-focus focus:ring-4 focus:ring-primary-300 focus:outline-none">
+          <input type="checkbox" 
+            checked={isPlaying}
+            onChange={togglePlay}
+          />
+          <svg className="swap-on fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M14,3.23V5.29C16.89,6.15 19,8.83 19,12C19,15.17 16.89,17.84 14,18.7V20.77C18,19.86 21,16.28 21,12C21,7.72 18,4.14 14,3.23M16.5,12C16.5,10.23 15.5,8.71 14,7.97V16C15.5,15.29 16.5,13.76 16.5,12M3,9V15H7L12,20V4L7,9H3Z"/></svg>
+          <svg className="swap-off fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M3,9H7L12,4V20L7,15H3V9M16.59,12L14,9.41L15.41,8L18,10.59L20.59,8L22,9.41L19.41,12L22,14.59L20.59,16L18,13.41L15.41,16L14,14.59L16.59,12Z"/></svg>
+        </label>
+      </div>
+      <ReactAudioPlayer
+        src={bgSound}
+        loop
+        autoPlay={autoplay}
+        id="bg-sound"
+        ref={player}
+      />
     </>
   )
 }
---
+++ src/sections/BukuTamu.jsx
@@ -0,0 +1,125 @@
+import { useState, useEffect } from 'react'
+import ReactTimeAgo from 'react-time-ago'
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient("https://akvlznzubtoxzjdqhlig.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFrdmx6bnp1YnRveHpqZHFobGlnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU2MTUxOTgsImV4cCI6MjAxMTE5MTE5OH0.heCj3HDDYVUK386lXUp8sIFnAfncV7MreuAKEiOOrS4");
+
+export default function BukuTamu() {
+  const [bukutamu, setBukutamu] = useState([]);
+  const [nama, setNama] = useState('');
+  const [ucapan, setUcapan] = useState('');
+  const [hadir, setHadir] = useState(1);
+  const [errorMsg, setErrorMsg] = useState('');
+  const [msg, setMsg] = useState('');
+
+  const fetchAll = async () => {
+    const { data, error } = await supabase.from('ucapan').select('*').order('created_at', { ascending: false })
+    if (data) setBukutamu(data);
+    if (error) setErrorMsg("Gagal mengambil data");
+  };
+
+  const addNew = async () => {
+    const { data, error } = await supabase
+      .from('ucapan')
+      .insert([
+        { nama: nama, ucapan: ucapan, hadir: hadir }
+      ])
+      .select()
+
+      setNama('');
+      setUcapan('');
+      setHadir(1);
+
+    if (data) {
+      // setBukutamu(values=>([...values, data]))
+      fetchAll()
+      // setIsAdded(true)
+      setMsg("Pesan terkirim");
+    }
+    if (error) {
+      setErrorMsg(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchAll();
+    console.log(bukutamu)
+  }, []);
+
+  // useEffect(() => {
+  //   setIsAdded(false)
+  // }, [isAdded]);
+
+  return(
+    <>
+      <div className="grid place-items-center text-center sm:px-2">
+        <div className="card md:w-96 w-full py-3 bg-base-100 shadow-lg my-5">
+          <div className="flex items-center justify-center">
+            <div className="flex flex-col">
+              <div className="card-body">
+                <h1 className="text-xl underline decoration-wavy font-body font-bold my-5 mb-10">Ucapan ({bukutamu.length})</h1>
+                <div className="form-control w-64">
+                  <label className="label">
+                    <span className="label-text">Nama?</span>
+                  </label>
+                  <input
+                    type="text" 
+                    value={nama}
+                    onChange={(e) => setNama(e.target.value)}
+                    placeholder="Masukan nama kamu" className="input input-bordered rounded-md w-full max-w-sm" />
+                </div>
+                <div className="form-control w-full">
+                  <label className="label">
+                    <span className="label-text">Pesan?</span>
+                  </label>
+                  <textarea placeholder="tulis ucapan kamu disini..." className="textarea textarea-bordered rounded-md w-full max-w-sm" onChange={(e) => setUcapan(e.target.value)} value={ucapan} />
+                </div>
+                <div className="form-control w-full">
+                  <label className="label">
+                    <span className="label-text">Kamu hadir kan?</span>
+                  </label>
+                  <select className="select select-bordered rounded-md w-full max-w-sm" value={hadir} onChange={(e) => setHadir(e.target.value)}>
+                    <option value="1">Insya Allah Hadir</option>
+                    <option value="0">Maaf, Belum bisa hadir</option>
+                  </select>
+                </div>
+                <div className="">
+                  <button className="btn btn-primary" onClick={addNew}>Kirim Ucapan</button>
+                </div>
+                <hr/>
+                {bukutamu.length<1 ? (
+                <span className="text-gray-300">Belum Ada Ucapan</span>
+                ):(
+                <div className="max-h-96 w-full overflow-y-scroll">
+                {bukutamu.map((p) => (
+                  <div className="chat chat-start my-2" key={p.id}>
+                    <div className="chat-image avatar">
+                      <div className="w-10 rounded-full">
+                        <img src={`https://ui-avatars.com/api/?name=${p.nama}&background=random`} />
+                      </div>
+                    </div>
+                    <div className="chat-header">
+                      {p.nama} 
+                      <time className="text-xs opacity-50 pl-1">
+                        {/* <ReactTimeAgo date={p.created_at} /> */}
+                        <ReactTimeAgo date={Date.parse(p.created_at)} locale="id-ID" timeStyle="twitter"/>
+                      </time>
+                    </div>
+                    <div className="chat-bubble text-left text-sm">{p.ucapan}</div>
+                    { p.hadir===true ? (
+                      <div className="chat-footer text-primary">
+                        Insya Allah hadir
+                      </div>
+                    ):('')}
+                  </div>
+                ))}
+                </div>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
