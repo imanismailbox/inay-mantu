08aa10f4760ea2b0302e2ab42c74bb978964951c
iman ismail <imanismailbox@gmail.com>
2023-10-06 19:50:43 +0700

    add footer text

--- src/sections/Penutup.jsx
+++ src/sections/Penutup.jsx
@@ -30,6 +30,13 @@ const Penutup = ({data}) => {
             </a>
           </div>
         </div>
+        <div className="font-body mb-3">
+          <span>
+            Made with &nbsp;
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-heart-fill text-red-500 inline" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+            </svg>&nbsp; in Jogja. by <a className="font-display text-primary font-bold" href="https://instagram.com/imanismailbox">Liamsi Nami</a></span>
+        </div>
       </div>
     </>
   )
