738c1269ba8d62c3e08e0e8f69b78403a6760d3d
iman ismail <imanismailbox@gmail.com>
2023-11-10 16:52:45 +0700

    Update BukuTamu.jsx

--- src/sections/BukuTamu.jsx
+++ src/sections/BukuTamu.jsx
@@ -69,10 +69,10 @@ export default function BukuTamu() {
             <div className="flex flex-col">
               <div className="card-body">
                 {nameNull==true ? (
-                <div className="alert alert-error">
-                  <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-                  <span>Pesan tidak terkirim!</span>
-                </div>
+                  <div className="alert alert-warning">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
+                    <span>Silahkan masukan Nama Anda!</span>
+                  </div>
                 ):(<></>)}
                 <h1 className="text-xl underline decoration-wavy font-body font-bold my-5 mb-10">Ucapan ({bukutamu.length})</h1>
                 <div className="form-control w-full">
